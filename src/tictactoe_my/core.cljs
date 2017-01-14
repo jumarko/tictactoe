7e932a2c13e8185e27a72120b31bd035f15a2352
Juraj Martinka <juraj@snapshot.travel>
2017-01-14 18:33:30 +0100

    Final game.
    Several steps have been skipped.
    The final source code copied from https://github.com/timothypratley/tictactoe/blob/master/src/tictactoe/core.cljs#L128
    -> just single fix: adding handler for :in-progress to the case

--- src/tictactoe_my/core.cljs
+++ src/tictactoe_my/core.cljs
@@ -3,62 +3,131 @@
 
 (enable-console-print!)
 
-(println "I changed some test")
-
-;; define your app data so that it doesn't get over-written on reload
-
 (defn new-board [n]
   (vec (repeat n (vec (repeat n "B")))))
 
-(def board-size 9)
+(def board-size 3)
 
-(defonce app-state (atom {:text "Welcome to tic tac toe"
-                          :board (new-board board-size)}))
+(def win-length 3)
 
-(defn computer-move []
-  (let [board (:board @app-state)
-        remaining-spots (for [i (range board-size)
+(defonce app-state
+  (atom {:text "Welcome to tic tac toe"
+         :board (new-board board-size)
+         :game-status :in-progress}))
+
+(defn computer-move [board]
+  (let [remaining-spots (for [i (range board-size)
                               j (range board-size)
-                              :when (= "B" (get-in board [j i]))]
-                          [i j])
-        move (rand-nth remaining-spots)
-        path (into [:board] (reverse move))]
-    (swap! app-state assoc-in path "C")))
+                              :when (= (get-in board [j i]) "B")]
+                          [j i])
+        move (when (seq remaining-spots)
+               (rand-nth remaining-spots))]
+    (if move
+      (assoc-in board move "C")
+      board)))
+
+#_(deftest computer-move-test
+    (is (= [["C"]]
+          (computer-move [["B"]])))
+    (is (= [["P"]]
+          (computer-move [["P"]]))))
+
+(defn straight [owner board [x y] [dx dy] n]
+  (every? true?
+    (for [i (range n)]
+      (= (get-in board [(+ (* dx i) x)
+                        (+ (* dy i) y)])
+        owner))))
+
+(defn win? [owner board n]
+  (some true?
+    (for [i (range board-size)
+          j (range board-size)
+          dir [[1 0] [0 1] [1 1] [1 -1]]]
+      (straight owner board [i j] dir n))    ))
+
+#_(deftest win?-test
+    (is (win? "P" [["P"]] 1))
+    (is (not (win? "P" [["P"]] 2)))
+    (is (win? "P" [["C" "P"]
+                   ["P" "C"]] 2)))
+
+(defn full? [board]
+  (every? #{"P" "C"} (apply concat board)))
+
+#_(deftest full?-test
+    (is (not (full? [["P" "B"]])))
+    (is (full? [["P" "C"]])))
+
+(defn game-status [board]
+  (cond
+    (win? "P" board win-length) :player-victory
+    (win? "C" board win-length) :computer-victory
+    (full? board) :draw
+    :else :in-progress))
+
+(defn update-status [state]
+  (assoc state :game-status (game-status (:board state))))
+
+(defn check-game-status [state]
+  (-> state
+    (update-in [:board] computer-move)
+    (update-status)))
 
 (defn blank [i j]
-  [:rect {:width 0.9
-          :height 0.9
-          :fill "grey"
-          :x (+ 0.05 i)
-          :y (+ 0.05 j)
-          :on-click
-          (fn rect-click [e]
-            (swap! app-state assoc-in [:board j i] "P")
-            (computer-move))}])
+  [:rect
+   {:width 0.9
+    :height 0.9
+    :fill "grey"
+    :x (+ 0.05 i)
+    :y (+ 0.05 j)
+    :on-click
+    (fn blank-click [e]
+      (when (= (:game-status @app-state) :in-progress)
+        (swap! app-state assoc-in [:board j i] "P")
+        (if (win? "P" (:board @app-state) win-length)
+          (swap! app-state assoc :game-status :player-victory)
+          (swap! app-state check-game-status))))}])
 
 (defn circle [i j]
   [:circle
-   {:r 0.45
-    :fill "green"
+   {:r 0.35
+    :stroke "green"
+    :stroke-width 0.12
+    :fill "none"
     :cx (+ 0.5 i)
     :cy (+ 0.5 j)}])
 
 (defn cross [i j]
   [:g {:stroke "darkred"
-       :stroke-width 0.25
+       :stroke-width 0.4
        :stroke-linecap "round"
        :transform
-       (str "translate(" (+ 0.25 i) "," (+ 0.25 j) ") "
-         "scale(0.5)")}
-   [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
-   [:line {:x1 1 :y1 0 :x2 0 :y2 1}]])
+       (str "translate(" (+ 0.5 i) "," (+ 0.5 j) ") "
+         "scale(0.3)")}
+   [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
+   [:line {:x1 1 :y1 -1 :x2 -1 :y2 1}]])
 
 (defn tictactoe []
   [:center
    [:h1 (:text @app-state)]
-   (into 
+   [:h2
+    (case (:game-status @app-state)
+      :player-victory "You won! "
+      :computer-victory "Computer wins. "
+      :draw "Draw. "
+      :in-progress "..."
+      """")
+    [:button
+     {:on-click
+      (fn new-game-click [e]
+        (swap! app-state assoc
+          :board (new-board board-size)
+          :game-status :in-progress))}
+     "New Game"]]
+   (into
      [:svg
-      {:view-box (str  "0 0 " board-size " " board-size)
+      {:view-box (str "0 0 " board-size " " board-size)
        :width 500
        :height 500}]
      (for [i (range board-size)
@@ -66,21 +135,10 @@
        (case (get-in @app-state [:board j i])
          "B" [blank i j]
          "P" [circle i j]
-         "C" [cross i j])))
-   [:p
-    [:button
-     {:on-click
-      (fn new-game-click [e]
-        (swap! app-state assoc :board (new-board board-size)))}
-     "New Game"]]]
-  )
+         "C" [cross i j])))])
 
 (reagent/render-component [tictactoe]
   (. js/document (getElementById "app")))
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  ;; (swap! app-state assoc-in [:board 0 0] 2)
-  )
+  (prn (:board @app-state)))

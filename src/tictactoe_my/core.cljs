60f756ea82e2257f86c89f7ab9ef506de3f5e27a
Juraj Martinka <juraj@snapshot.travel>
2017-01-14 17:17:04 +0100

    Add click handler for board tiles

--- src/tictactoe_my/core.cljs
+++ src/tictactoe_my/core.cljs
@@ -16,18 +16,22 @@
 (defn tictactoe []
   [:center
    [:h1 (:text @app-state)]
-   [:svg
-    {:view-box "0 0 3 3"
-     :width 500
-     :height 500}
-    (for [i (range (count (:board @app-state)))
-          j (range (count (:board @app-state)))]
-      [:rect {:width 0.9
-              :height 0.9
-              :fill "green"
-              :x i
-              :y j}])
-    ]])
+   (into 
+     [:svg
+      {:view-box "0 0 3 3"
+       :width 500
+       :height 500}]
+     (for [i (range (count (:board @app-state)))
+           j (range (count (:board @app-state)))]
+       [:rect {:width 0.9
+               :height 0.9
+               :fill "green"
+               :x i
+               :y j
+               :on-click
+               (fn rect-click [e]
+                 (prn "You clicked me!" i j)
+                 (prn (swap! app-state update-in [:board i j] inc)))}]))])
 
 (reagent/render-component [tictactoe]
   (. js/document (getElementById "app")))

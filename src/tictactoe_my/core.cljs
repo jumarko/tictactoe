66082b4ff8ab6bc75784086950481df190e7e03a
Juraj Martinka <juraj@snapshot.travel>
2017-01-14 15:54:29 +0100

    Add basic svg circle

--- src/tictactoe_my/core.cljs
+++ src/tictactoe_my/core.cljs
@@ -7,13 +7,20 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Welcome to tic tac toe"}))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn tictactoe []
+  [:center
+   [:h1 (:text @app-state)]
+   [:svg
+    {:view-box "0 0 30 30"
+     :width 500
+     :height 500}
+    [:circle {:r 30 :cx 30 :cy 30}]]
+   ])
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+(reagent/render-component [tictactoe]
+  (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
